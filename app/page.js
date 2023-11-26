002dc5214502b1597f3090df7f98abd9de83ded0
Prabuddha Chatterjee <prabuddhapiku@gmail.com>
2023-11-26 12:04:05 +0530

    added payment method

--- app/page.js
+++ app/page.js
@@ -6,11 +6,64 @@ import { ImQuotesRight } from "react-icons/im";
 
 export default function Home() {
   const [animationClass, setAnimationClass] = useState('');
-  const [age, setAge] = useState(0);
-  const [height, setHeight] = useState(0);
-  const [weight, setWeight] = useState(0);
+  const [age, setAge] = useState(20);
+  const [height, setHeight] = useState(160);
+  const [weight, setWeight] = useState(60);
   const [gender, setGender] = useState('male'); // Default to male
   const [calculatedValue, setCalculatedValue] = useState(0);
+
+
+  const handleDownload = () => {
+    const imageUrl = '/scanner.jpg'; // Replace with the actual path or URL of your image
+    const downloadLink = document.createElement('a');
+    downloadLink.href = imageUrl;
+    downloadLink.download = 'payment_scanner.jpg'; // Specify the desired filename
+    downloadLink.click();
+  };
+
+
+
+  const [isDivOpen, setIsDivOpen] = useState(false);
+  const [countdown, setCountdown] = useState(300); // 300 seconds = 5 minutes
+
+  useEffect(() => {
+    let countdownInterval;
+
+    if (isDivOpen) {
+      countdownInterval = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000); // Update countdown every second
+    }
+
+    return () => {
+      clearInterval(countdownInterval);
+    };
+  }, [isDivOpen]);
+
+  useEffect(() => {
+    if (countdown <= 0) {
+      setIsDivOpen(false);
+    }
+  }, [countdown]);
+
+  const handleDivClick = () => {
+    setIsDivOpen(!isDivOpen);
+    setCountdown(300);
+  };
+
+  const handleCloseClick = () => {
+    setIsDivOpen(false);
+    setCountdown(300);
+  };
+
+  const formatTime = (time) => {
+    return time < 10 ? `0${time}` : time;
+  };
+
+  const minutes = Math.floor(countdown / 60);
+  const seconds = countdown % 60;
+
+
 
   useEffect(() => {
     // Add a delay to start the animation after the component is mounted
@@ -24,12 +77,12 @@ export default function Home() {
   useEffect(() => {
     // Calculate maintenance calorie using the provided formula
     const calculatedResult = calculateMaintenanceCalorie();
-    setCalculatedValue(calculatedResult);
+    const calculatedResultRounded = Math.round(calculatedResult);
+    setCalculatedValue(calculatedResultRounded);
   }, [age, height, weight, gender]);
 
   const calculateMaintenanceCalorie = () => {
-    const baseFormula = 10 * weight + 6.25 * height - 5 * age;
-    return gender === 'male' ? baseFormula + 5 : baseFormula - 161;
+    return gender === 'male' ? 66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age) : 655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age);
   };
 
   const Typewriter = ({ text, delay }) => {
@@ -79,13 +132,61 @@ export default function Home() {
           >
             <Image src="/logo.png" alt="logo" width={80} height={80} />
             <div className='text-black text-3xl font-bold'>
-              <Typewriter text="Calorie Guard" delay={300} />
+              <h1><Typewriter text="Calorie Guard" delay={300} /></h1>
             </div>
           </div>
-          <div className='text-black w-[10%] mt-5 ms-[40%] flex flex-col justify-center items-center'>
-            <img width="44" height="44" src="https://img.icons8.com/external-flaticons-flat-flat-icons/64/external-early-bird-cyber-monday-flaticons-flat-flat-icons.png" alt="external-early-bird-cyber-monday-flaticons-flat-flat-icons" />
+
+          <div
+            style={{
+              position: 'fixed',
+              top: '0',
+              left: '0',
+              width: '100%',
+              height: '100%',
+              background: 'rgba(0, 0, 0, 0.5)', // semi-transparent black overlay
+              zIndex: isDivOpen ? '999' : '-1', // show overlay only when isDivOpen is true
+              display: isDivOpen ? 'block' : 'none', // hide overlay when isDivOpen is false
+            }}
+          ></div>
+
+          <div className='text-black w-[10%] mt-5 ms-[40%] flex flex-col justify-center items-center' onClick={handleDivClick} style={{ cursor: 'pointer' }}>
+            <img
+              width="44"
+              height="44"
+              src="https://img.icons8.com/external-flaticons-flat-flat-icons/64/external-early-bird-cyber-monday-flaticons-flat-flat-icons.png"
+              alt="external-early-bird-cyber-monday-flaticons-flat-flat-icons"
+            />
             <p>Donate Now</p>
             <p>Support Us</p>
+
+            {isDivOpen && (
+              <div
+                style={{
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  background: 'white',
+                  padding: '20px',
+                  boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+                  zIndex: 999,
+                }}
+              >
+                <span style={{ float: 'right', cursor: 'pointer' }} onClick={handleCloseClick}>
+                  &#10006;
+                </span>
+                <div className='text-center'>
+                  <strong>SCAN to PAY</strong>
+                  <Image src="/UPI.png" width={100} height={20} />
+                </div>
+                <div>
+                  <Image src="/scanner.jpg" alt='Payment Scanner' height={300} width={300} />
+                </div>
+                <div className='text-center'>
+                  Time remaining: <strong>{formatTime(minutes)}:{formatTime(seconds)}</strong>
+                </div>
+              </div>
+            )}
           </div>
           <div className='me-20 ms-5'>
             <Image src="/playstore.png" height={5} width={150} />
@@ -124,16 +225,24 @@ export default function Home() {
             <h2 className="text-2xl font-bold mb-5">Calculate Your Maintenance Calorie</h2>
             <div className="flex flex-col">
               <label htmlFor="age" className="mb-2">Age:</label>
-              <input type="number" id="age" name="age" className="p-2 mb-4 input-border" onChange={(e) => setAge(e.target.value)} />
+              <input type="number" id="age" name="age" value={age} className="p-2 mb-4 input-border" onChange={(e) => setAge(e.target.value)} />
 
-              <label htmlFor="height" className="mb-2">Height (in cm):</label>
-              <input type="range" id="height" name="height" min="50" max="300" className="mb-2 input-border" onChange={(e) => setHeight(e.target.value)} />
-              <span>{`Selected Height: ${height} cm`}</span>
+              <label htmlFor="height" className="mb-2 mt-5">Height : <span>{`${height} cm`}</span></label>
+              <input
+                type="range"
+                id="height"
+                name="height"
+                min="50"
+                max="300"
+                className=''
+                style={{ width: '100%' }}
+                onChange={(e) => setHeight(e.target.value)}
+              />
 
-              <label htmlFor="weight" className="mb-2">Weight (in kg):</label>
-              <input type="number" id="weight" name="weight" className="p-2 mb-4 input-border" onChange={(e) => setWeight(e.target.value)} />
+              <label htmlFor="weight" className="mb-2 mt-8">Weight (in kg):</label>
+              <input type="number" id="weight" name="weight" value={weight} className="p-2 mb-4 input-border" onChange={(e) => setWeight(e.target.value)} />
 
-              <div className="mb-2">Gender:</div>
+              <div className="mb-2 mt-5">Gender:</div>
               <div className="flex mb-4">
                 <div
                   className="gender-option"
@@ -154,11 +263,11 @@ export default function Home() {
               <button className="bg-orange-700 text-white py-2 px-4 rounded">Calculate</button>
             </div>
 
-            {/* Display the calculated value */}
             <div className="mt-4">
-              <strong>Calculated Value:</strong> {calculatedValue}
+              <strong>Maintenance Calorie :</strong> {calculatedValue}<strong> cals</strong>
             </div>
           </section>
+          <iframe src="https://www.figma.com/proto/FGekHHIrlAKAJyrYdMAyDo/Untitled?type=design&node-id=3-2&t=HXhmOpNqbl3TL8q6-1&scaling=scale-down&page-id=0%3A1&mode=design" width="800" height="600" frameborder="0" allowfullscreen></iframe>
         </div>
 
         <style jsx>{`
@@ -274,13 +383,56 @@ export default function Home() {
           <div className='w-screen flex justify-center'>
             <Image src="/down.gif" height={60} width={60} className='me-40' />
           </div>
+
+          <div
+            style={{
+              position: 'fixed',
+              top: '0',
+              left: '0',
+              width: '100%',
+              height: '100%',
+              background: 'rgba(0, 0, 0, 0.5)', // semi-transparent black overlay
+              zIndex: isDivOpen ? '999' : '-1', // show overlay only when isDivOpen is true
+              display: isDivOpen ? 'block' : 'none', // hide overlay when isDivOpen is false
+            }}
+          ></div>
+
           <div className='relative'>
             <div className='absolute top-0 text-black w-[80%] bg-white ms-10 mt-5 flex flex-row justify-between items-center border rounded-xl'>
-              <div className='ms-10 bg-orange-700 text-white p-5'>DONATE NOW</div>
+              <div className='ms-10 bg-orange-700 text-white p-5' onClick={handleDivClick}>DONATE NOW</div>
               <div className='felx flex-col me-5 py-5'>
                 <img width="44" height="44" src="https://img.icons8.com/external-flaticons-flat-flat-icons/64/external-early-bird-cyber-monday-flaticons-flat-flat-icons.png" alt="external-early-bird-cyber-monday-flaticons-flat-flat-icons" />
                 <p>Support Us</p>
               </div>
+              {isDivOpen && (
+                <div
+                  style={{
+                    position: 'fixed',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    background: 'white',
+                    padding: '20px',
+                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+                    zIndex: 999,
+                  }}
+                >
+                  <span style={{ float: 'right', cursor: 'pointer' }} onClick={handleCloseClick}>
+                    &#10006;
+                  </span>
+                  <Image src="/UPI.png" width={60} height={20} alt='UPI' />
+                  <div>
+                    <Image src="/scanner.jpg" alt='Payment Scanner' height={400} width={400} />
+                  </div>
+                  <div className='text-center'><strong>SCAN & PAY<br />or</strong></div>
+                  <div className='text-center'>
+                    <button onClick={handleDownload} className='bg-orange-700 text-white rounded-xl p-2 mb-5'>Download QR</button>
+                  </div>
+                  <div className='text-center'>
+                    Time remaining: <strong>{formatTime(minutes)}:{formatTime(seconds)}</strong>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
           <div className='relative px-5'>
@@ -288,16 +440,24 @@ export default function Home() {
               <h2 className="text-2xl font-bold mb-5">Calculate Your Maintenance Calorie</h2>
               <div className="flex flex-col">
                 <label htmlFor="age" className="mb-2">Age:</label>
-                <input type="number" id="age" name="age" className="p-2 mb-4 input-border" onChange={(e) => setAge(e.target.value)} />
+                <input type="number" id="age" name="age" value={age} className="p-2 mb-4 input-border" onChange={(e) => setAge(e.target.value)} />
 
-                <label htmlFor="height" className="mb-2">Height (in cm):</label>
-                <input type="range" id="height" name="height" min="50" max="300" className="mb-2 input-border" onChange={(e) => setHeight(e.target.value)} />
-                <span>{`Selected Height: ${height} cm`}</span>
+                <label htmlFor="height" className="mb-2 mt-5">Height : <span>{`${height} cm`}</span></label>
+                <input
+                  type="range"
+                  id="height"
+                  name="height"
+                  min="50"
+                  max="300"
+                  className=''
+                  style={{ width: '100%' }}
+                  onChange={(e) => setHeight(e.target.value)}
+                />
 
-                <label htmlFor="weight" className="mb-2">Weight (in kg):</label>
-                <input type="number" id="weight" name="weight" className="p-2 mb-4 input-border" onChange={(e) => setWeight(e.target.value)} />
+                <label htmlFor="weight" className="mb-2 mt-8">Weight (in kg):</label>
+                <input type="number" id="weight" name="weight" value={weight} className="p-2 mb-4 input-border" onChange={(e) => setWeight(e.target.value)} />
 
-                <div className="mb-2">Gender:</div>
+                <div className="mb-2 mt-5">Gender:</div>
                 <div className="flex mb-4">
                   <div
                     className="gender-option"
@@ -318,9 +478,8 @@ export default function Home() {
                 <button className="bg-orange-700 text-white py-2 px-4 rounded">Calculate</button>
               </div>
 
-              {/* Display the calculated value */}
               <div className="mt-4">
-                <strong>Calculated Value:</strong> {calculatedValue}
+                <strong>Maintenance Calorie :</strong> {calculatedValue}<strong> cals</strong>
               </div>
             </section>
           </div>
