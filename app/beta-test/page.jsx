5260fbfce7d4e58f7b26c3a4282946651e117fa2
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-02-19 09:58:21 +0530

    Update page.jsx with beta test sign-up form

--- app/beta-test/page.jsx
+++ app/beta-test/page.jsx
@@ -1,22 +1,31 @@
+'use client'
 import React from 'react'
 import { Rajdhani } from "next/font/google";
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 const rajdhani = Rajdhani({ subsets: ["latin"], weight: ["500"] });
 const page = () => {
+  const router = useRouter()
   return (
-    <div className={`px-4 flex items-center justify-center w-full h-screen ${rajdhani.className} gap-10 text-4xl flex-col `}>
+    <div className={`px-4 flex items-center justify-center w-full h-screen ${rajdhani.className} gap-10 text-4xl flex-col bg-[#F8F8FF]`}>
+    <Image
+      src='/logo.png'
+      height={100}
+      width={150}
+      className='p-2 rounded-3xl shadow-xl bg-[#FAF9F6]'
+    />
         Coming Soon on 
         <Image
           src="/playstore.png"
           height={5} width={150} 
         />
         <p>
-          For early access sign up for our beta test
+          For early access sign up for our <strong>Beta Test Programme</strong>
         </p>
-        <Link href=''>
-        <Button className='text-3xl'>Beta Test</Button>
+        <Link href='https://forms.gle/RdZBWhehhhK2vQHc6' target='_blank' onClick={()=>router.push('/')}>
+        <Button className='bg-[#0d0d0d] text-3xl text-white rounded-md'>Sign Up!</Button>
         </Link>
     </div>
   )
