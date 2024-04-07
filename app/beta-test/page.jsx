d6260cb660f2b7c54e77d7e36ec74b949305464e
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-04-08 00:01:09 +0530

    Update import path for Button component in page.jsx

--- app/beta-test/page.jsx
+++ app/beta-test/page.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { Rajdhani } from "next/font/google";
 import Image from 'next/image';
-import { Button } from '@/components/ui/button';
+import { Button } from '../../components/ui/button';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 const rajdhani = Rajdhani({ subsets: ["latin"], weight: ["500"] });
